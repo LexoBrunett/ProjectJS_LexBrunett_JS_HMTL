c8a8ffd49335152ef991a3950a266158de7f466f
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-08-04 21:15:35 -0500

    js corrections

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import "index.html"
 import "styles.css"
 
-window.onload = function(){
+window.onload = function() {
 	let who = ["The dog", "My grandma", "His turtle", "My bird"];
 	let action = ["ate", "peed", "crushed", "broke"];
 	let what = ["my homework", "the keys", "the car"];
 	let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
 
-	let excusa =who[rand(who.length)] +" " +action[rand(action.length)]+ " " +what[rand(what.length)]+ " " +when[rand(when.length)] ;
+	let sentence =who[rand(who.length)] +" " +action[rand(action.length)]+ " " +what[rand(what.length)]+ " " +when[rand(when.length)] ;
 
 
-	let excusep = document.querySelector("#excuse");
-	excusep.innerHTML = excusa;
-}
+	let excuseSpan = document.querySelector("#excuse");
+	excuseSpan.innerHTML = excusa;
+};
 
 function rand(length) {
 	let number = Math.floor(Math.random() * length);
