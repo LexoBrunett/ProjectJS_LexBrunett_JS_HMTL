2be0e81b89e879ba580f39eacda8df01c88dffd8
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-08-04 21:09:29 -0500

    Html, js and css creation

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
+import "index.html"
+import "styles.css"
 
-let domain;
-function generatedomain() {
-	let who_word = Math.random() * who.length;
-	let action_word = Math.random() * action.length;
-	let what_word = Math.random() * what.length;
-	let when_word = Math.random() * when.length;
+window.onload = function(){
+	let who = ["The dog", "My grandma", "His turtle", "My bird"];
+	let action = ["ate", "peed", "crushed", "broke"];
+	let what = ["my homework", "the keys", "the car"];
+	let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
 
-	domain =who[who_word] + action[action_word] + what[what_word] + when[when_word];
+	let excusa =who[rand(who.length)] +" " +action[rand(action.length)]+ " " +what[rand(what.length)]+ " " +when[rand(when.length)] ;
 
-	return domain;
+
+	let excusep = document.querySelector("#excuse");
+	excusep.innerHTML = excusa;
 }
+
+function rand(length) {
+	let number = Math.floor(Math.random() * length);
+	return number;
+	}
+
