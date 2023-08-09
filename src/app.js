fa192fbf0b3ac529d16b63b58dfd30a4ac660d88
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-08-09 23:11:50 +0000

    html corrections

--- src/app.js
+++ src/app.js
@@ -1,27 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-import "index.html"
-import "styles.css"
+import "index.html";
+import "styles.css";
 
 window.onload = function() {
-	let who = ["The dog", "My grandma", "His turtle", "My bird"];
-	let action = ["ate", "peed", "crushed", "broke"];
-	let what = ["my homework", "the keys", "the car"];
-	let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-	let sentence =who[rand(who.length)] +" " +action[rand(action.length)]+ " " +what[rand(what.length)]+ " " +when[rand(when.length)] ;
+  let sentence =
+    who[rand(who.length)] +
+    " " +
+    action[rand(action.length)] +
+    " " +
+    what[rand(what.length)] +
+    " " +
+    when[rand(when.length)];
 
-
-	let excuseSpan = document.querySelector("#excuse");
-	excuseSpan.innerHTML = excusa;
+  let excuseSpan = document.querySelector("#excuse");
+  excuseSpan.innerHTML = excusa;
 };
 
 function rand(length) {
-	let number = Math.floor(Math.random() * length);
-	return number;
-	}
-
+  let number = Math.floor(Math.random() * length);
+  return number;
+}
